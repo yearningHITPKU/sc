7fa398cc27488baeea15e315e088e939a769200a
yearning <hit_xiangwang@163.com>
2018-08-09 17:20:38 +0800

    修改jar包路径

--- smartcontract.go
+++ smartcontract.go
@@ -8,7 +8,7 @@ import (
 func StartContract(arg string) (outStr string, err error) {
 	// example:
 	// cmd := exec.Command("java", "-classpath", "/home/yearning/eclipse-workspace/yjs.jar", "com.yancloud.sc.SCAPI", "startContract", "{\"type\":\"Data\",\"id\":\"656564\"}")
-	cmd := exec.Command("java", "-classpath", "/home/yearning/eclipse-workspace/yjs.jar", "com.yancloud.sc.SCAPI", "startContract", arg)
+	cmd := exec.Command("java", "-classpath", "./yjs.jar", "com.yancloud.sc.SCAPI", "startContract", arg)
 
 	fmt.Println(cmd.Path, cmd.Args)
 	out, err := cmd.Output()
@@ -20,12 +20,12 @@ func StartContract(arg string) (outStr string, err error) {
 	return
 }
 
-func execContract(arg string) (outStr string, err error) {
+func ExecContract(arg string) (outStr string, err error) {
 	// example:
 	// cmd := exec.Command("java", "-classpath", "/home/yearning/eclipse-workspace/yjs.jar", "com.yancloud.sc.SCAPI", "approveContract", "{\"arg\":\"http://www.baidu.com\",\"contractID\":\"656564\"}")
 	cmd := exec.Command(
 		"java", "-classpath",
-		"/home/yearning/eclipse-workspace/yjs.jar",
+		"./yjs.jar",
 		"com.yancloud.sc.SCAPI", "approveContract",
 		arg)
 
