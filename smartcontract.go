52da583d97d83349136483b9133fea960daf67d3
yearning <hit_xiangwang@163.com>
2018-08-09 16:49:41 +0800

    增加签署合约和执行合约接口

--- smartcontract.go
+++ smartcontract.go
@@ -1,2 +1,40 @@
 package sc
 
+import (
+	"fmt"
+	"os/exec"
+)
+
+func StartContract(arg string) (outStr string, err error) {
+	// example:
+	// cmd := exec.Command("java", "-classpath", "/home/yearning/eclipse-workspace/yjs.jar", "com.yancloud.sc.SCAPI", "startContract", "{\"type\":\"Data\",\"id\":\"656564\"}")
+	cmd := exec.Command("java", "-classpath", "/home/yearning/eclipse-workspace/yjs.jar", "com.yancloud.sc.SCAPI", "startContract", arg)
+
+	fmt.Println(cmd.Path, cmd.Args)
+	out, err := cmd.Output()
+	if err != nil {
+		fmt.Println(err)
+	}
+	outStr = string(out)
+	fmt.Print("aaaaaa", outStr)
+	return
+}
+
+func execContract(arg string) (outStr string, err error) {
+	// example:
+	// cmd := exec.Command("java", "-classpath", "/home/yearning/eclipse-workspace/yjs.jar", "com.yancloud.sc.SCAPI", "approveContract", "{\"arg\":\"http://www.baidu.com\",\"contractID\":\"656564\"}")
+	cmd := exec.Command(
+		"java", "-classpath",
+		"/home/yearning/eclipse-workspace/yjs.jar",
+		"com.yancloud.sc.SCAPI", "approveContract",
+		arg)
+
+	fmt.Println(cmd.Path, cmd.Args)
+	out, err := cmd.Output()
+	if err != nil {
+		fmt.Println(err)
+	}
+	outStr = string(out)
+	fmt.Print("aaaaaa", string(out))
+	return
+}
